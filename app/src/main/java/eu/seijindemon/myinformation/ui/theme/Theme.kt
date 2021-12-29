2672fb6cb6fa810a55800f82c3d9ece418199a0a
SeijinD <seijindemon@protonmai.com>
2021-12-29 21:14:26 +0200

    Test theme

--- app/src/main/java/eu/seijindemon/myinformation/ui/theme/Theme.kt
+++ app/src/main/java/eu/seijindemon/myinformation/ui/theme/Theme.kt
@@ -5,7 +5,11 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 
 @SuppressLint("ConflictingOnColor")
@@ -42,16 +46,21 @@ fun MyInformationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+
+    val rippleCustomTheme = object : RippleTheme {
+        @Composable
+        override fun defaultColor() = RippleTheme.defaultRippleColor(Color.Black, lightTheme = true)
+
+        @Composable
+        override fun rippleAlpha(): RippleAlpha = RippleTheme.defaultRippleAlpha(Color.Black, lightTheme = true)
     }
+
 
     MaterialTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
-        content = content
+        content = { CompositionLocalProvider(LocalRippleTheme provides rippleCustomTheme) { content() } }
     )
 }
