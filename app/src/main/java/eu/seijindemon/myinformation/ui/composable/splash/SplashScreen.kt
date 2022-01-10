1dd8747331a744918f2439a1265c8eb1f94881d9
gkaranikolas <gkaranikolas@arx.net>
2022-01-10 10:39:53 +0200

    Update SplashScreen

--- app/src/main/java/eu/seijindemon/myinformation/ui/composable/splash/SplashScreen.kt
+++ app/src/main/java/eu/seijindemon/myinformation/ui/composable/splash/SplashScreen.kt
@@ -1,20 +1,17 @@
 package eu.seijindemon.myinformation.ui.composable.splash
 
 import android.content.res.Configuration
-import android.view.animation.OvershootInterpolator
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import eu.seijindemon.myinformation.R
@@ -33,37 +30,32 @@ fun SplashScreen(
 fun SplashContent(
     navController: NavController
 ) {
-    val scale = remember { Animatable(0f) }
+    var startAnimation by remember { mutableStateOf(false) }
+    val alphaAnim = animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 3000
+        )
+    )
     
     LaunchedEffect(key1 = true) {
-        scale.animateTo(
-            targetValue = 0.5f,
-            animationSpec = tween(
-                durationMillis = 500,
-                easing = {
-                    OvershootInterpolator(2f).getInterpolation(it)
-                }
-            )
-        )
-        delay(2000L)
-    }
-
-    LaunchedEffect(key1 = true) {
-        delay(4000L)
+        startAnimation = true
+        delay(4000)
+        navController.popBackStack()
         navController.navigate("home")
     }
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
         Image(
             painter = painterResource(id = R.drawable.logo),
             contentDescription = "Splash Image",
             modifier = Modifier
-                .fillMaxWidth()
-                .scale(scale.value)
+                .alpha(alpha = alphaAnim.value)
+                .padding(all = 30.dp)
+                .fillMaxSize()
         )
     }
 }
